f04d9fdba53af26abf28aa02f565381188c07951
hoonjoo94 <hoonjoo94@naver.com>
2022-11-29 19:52:39 +0900

    TodoList와 Task 모델 생성 및 관계 설정

---
+++ mohani/Model/Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  mohani
+//
+//  Created by Hoonjoo Park on 2022/11/29.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
---
+++ mohani/Model/TodoList+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TodoList+CoreDataClass.swift
+//  mohani
+//
+//  Created by Hoonjoo Park on 2022/11/29.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TodoList)
+public class TodoList: NSManagedObject {
+
+}
