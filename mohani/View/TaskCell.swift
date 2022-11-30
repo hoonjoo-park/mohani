643efa8b248b4b1433654ed1fbb7818b757862a4
hoonjoo94 <hoonjoo94@naver.com>
2022-12-01 04:56:04 +0900

    TaskCell 테이블뷰셀 마크업

--- mohani/View/TaskCell.swift
+++ mohani/View/TaskCell.swift
@@ -10,19 +10,59 @@ import UIKit
 class TaskCell: UITableViewCell {
     static let reuseId = "TaskCell"
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
+    var checkBoxButton = UIButton()
+    let bodyLabel = BodyLabel(color: Colors.black)
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureCellUI()
+        configureUI()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     
     func setCell(task: Task) {
-        // TODO: task를 넘겨 받은 뒤 -> title과 isDone에 대한 처리를 수행
+        if task.isDone {
+            checkBoxButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .selected)
+        } else {
+            checkBoxButton.setImage(UIImage(systemName: "checkmark.circle.fillcirclebadge"), for: .normal)
+        }
+        
+        bodyLabel.text = task.title
         return
+    }
+    
+    
+    private func configureCellUI() {
+        let shadowColor = Colors.black.cgColor
+        backgroundColor = Colors.white
+        layer.cornerRadius = 10
+        layer.shadowColor = shadowColor
+        layer.shadowRadius = 10
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 0.15
+    }
+    
+    
+    private func configureUI() {
+        let padding: CGFloat = 15
+        addSubviews(checkBoxButton, bodyLabel)
+        
+        NSLayoutConstraint.activate([
+            checkBoxButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            checkBoxButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            checkBoxButton.widthAnchor.constraint(equalToConstant: 20),
+            checkBoxButton.heightAnchor.constraint(equalToConstant: 20),
+            
+            bodyLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            bodyLabel.leadingAnchor.constraint(equalTo: checkBoxButton.trailingAnchor, constant: 18),
+            bodyLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+        ])
     }
 
 }
