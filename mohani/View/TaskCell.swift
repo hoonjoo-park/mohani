0e6a2b7c885a09cb5315e2d7815dc3f324f1e614
hoonjoo94 <hoonjoo94@naver.com>
2022-12-24 19:20:12 +0900

    TaskCell 코드 리팩토링

--- mohani/View/TaskCell.swift
+++ mohani/View/TaskCell.swift
@@ -45,6 +45,66 @@ class TaskCell: UICollectionViewCell {
     }
     
     
+    private func configureCellUI() {
+        let shadowColor = Colors.black.cgColor
+        cellView.backgroundColor = Colors.white
+        cellView.layer.cornerRadius = 10
+        cellView.layer.shadowColor = shadowColor
+        cellView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        cellView.layer.shadowRadius = 3
+        cellView.layer.shadowOpacity = 0.12
+        
+        checkBoxButton.translatesAutoresizingMaskIntoConstraints = false
+        cellView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubviews(cellView, taskDeleteButton)
+        cellView.addSubviews(checkBoxButton, bodyLabel)
+    }
+    
+    
+    private func configureDeleteButton() {
+        taskDeleteButton.alpha = 0
+        taskDeleteButton.translatesAutoresizingMaskIntoConstraints = false
+        taskDeleteButton.image = UIImage(systemName: "trash.fill")
+        taskDeleteButton.tintColor = .systemRed
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(deleteTask))
+        taskDeleteButton.addGestureRecognizer(gestureRecognizer)
+        taskDeleteButton.isUserInteractionEnabled = true
+    }
+    
+    
+    private func configureUI() {
+        let padding: CGFloat = 15
+        
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: topAnchor),
+            cellView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            cellView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            cellView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            checkBoxButton.centerYAnchor.constraint(equalTo: cellView.centerYAnchor),
+            checkBoxButton.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: padding),
+            checkBoxButton.widthAnchor.constraint(equalToConstant: 20),
+            checkBoxButton.heightAnchor.constraint(equalToConstant: 20),
+            
+            bodyLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor),
+            bodyLabel.leadingAnchor.constraint(equalTo: checkBoxButton.trailingAnchor, constant: 10),
+            bodyLabel.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -padding),
+            
+            taskDeleteButton.heightAnchor.constraint(equalToConstant: 20),
+            taskDeleteButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            taskDeleteButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+        ])
+    }
+    
+    
+    private func addGestureHandler() {
+        self.addGestureRecognizer(self.gesture)
+        self.gesture.addTarget(self, action: #selector(self.handleSwipeGesture(_:)))
+    }
+    
+    
     func setCell(task: Task, indexPath: IndexPath) {
         self.task = task
         self.indexPath = indexPath
@@ -93,69 +153,11 @@ class TaskCell: UICollectionViewCell {
     }
     
     
-    private func configureDeleteButton() {
-        taskDeleteButton.alpha = 0
-        taskDeleteButton.translatesAutoresizingMaskIntoConstraints = false
-        taskDeleteButton.image = UIImage(systemName: "trash.fill")
-        taskDeleteButton.tintColor = .systemRed
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(deleteTask))
-        taskDeleteButton.addGestureRecognizer(gestureRecognizer)
-        taskDeleteButton.isUserInteractionEnabled = true
-    }
-    
-    
     @objc private func deleteTask() {
         delegate.onTapDeleteTask(indexPath: indexPath)
         transformToIdentity()
     }
     
-    
-    private func configureCellUI() {
-        let shadowColor = Colors.black.cgColor
-        cellView.backgroundColor = Colors.white
-        cellView.layer.cornerRadius = 10
-        cellView.layer.shadowColor = shadowColor
-        cellView.layer.shadowOffset = CGSize(width: 0, height: 2)
-        cellView.layer.shadowRadius = 3
-        cellView.layer.shadowOpacity = 0.12
-        
-        checkBoxButton.translatesAutoresizingMaskIntoConstraints = false
-        cellView.translatesAutoresizingMaskIntoConstraints = false
-        
-        addSubviews(cellView, taskDeleteButton)
-        cellView.addSubviews(checkBoxButton, bodyLabel)
-    }
-    
-    
-    private func configureUI() {
-        let padding: CGFloat = 15
-        
-        NSLayoutConstraint.activate([
-            cellView.topAnchor.constraint(equalTo: topAnchor),
-            cellView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            cellView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            cellView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            
-            checkBoxButton.centerYAnchor.constraint(equalTo: cellView.centerYAnchor),
-            checkBoxButton.leadingAnchor.constraint(equalTo: cellView.leadingAnchor, constant: padding),
-            checkBoxButton.widthAnchor.constraint(equalToConstant: 20),
-            checkBoxButton.heightAnchor.constraint(equalToConstant: 20),
-            
-            bodyLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor),
-            bodyLabel.leadingAnchor.constraint(equalTo: checkBoxButton.trailingAnchor, constant: 10),
-            bodyLabel.trailingAnchor.constraint(equalTo: cellView.trailingAnchor, constant: -padding),
-            
-            taskDeleteButton.heightAnchor.constraint(equalToConstant: 20),
-            taskDeleteButton.centerYAnchor.constraint(equalTo: centerYAnchor),
-            taskDeleteButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
-        ])
-    }
-    
-    private func addGestureHandler() {
-        self.addGestureRecognizer(self.gesture)
-        self.gesture.addTarget(self, action: #selector(self.handleSwipeGesture(_:)))
-    }
     
     @objc private func handleSwipeGesture(_ gesture: UIPanGestureRecognizer) {
         let translationX = gesture.translation(in: self).x
@@ -171,8 +173,6 @@ class TaskCell: UICollectionViewCell {
             } else {
                 taskDeleteButton.alpha = -translationX / 50
             }
-
-//            taskDeleteButton.alpha += -translationX / 100
         
         case .cancelled, .ended:
             guard translationX > threshold else {
@@ -202,6 +202,5 @@ class TaskCell: UICollectionViewCell {
         UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseIn) {
             self.cellView.transform = CGAffineTransform(translationX: -50, y: 0)
         }
-        taskDeleteButton.alpha = 1
     }
 }
